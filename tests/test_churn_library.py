4a9f986dce88547223dc9e971578fd289b84a724
abdulazizab2 <albuainaina8@gmail.com>
2023-05-29 20:45:24 +0300

    add more fixtures and refactor unit tests

--- tests/test_churn_library.py
+++ tests/test_churn_library.py
@@ -7,10 +7,11 @@
 # pylint: disable=redefined-outer-name
 import sys  # pylint: disable=wrong-import-position
 sys.path.insert(0, './ChurnRate')  # pylint: disable=wrong-import-position
-import pytest
-from ChurnRate import constants
-from ChurnRate.churn_library import read_data, perform_eda, encoder_helper, perform_feature_engineering
 import os
+import pytest
+import joblib
+from ChurnRate import constants
+from ChurnRate.churn_library import read_data, perform_eda, encoder_helper, perform_feature_engineering, train_models, feature_importance_plot, classification_report_image
 import pandas as pd
 
 
@@ -24,6 +25,18 @@ def df_fixture():
         churn_column_name = f"{column_name}_Churn"
         df[churn_column_name] = 0
     return df
+
+@pytest.fixture
+def data_fixture(df_fixture):
+    x_train, x_test, y_train, y_test, x_data = perform_feature_engineering(df_fixture)
+    return x_train, x_test, y_train, y_test, x_data
+
+@pytest.fixture
+def models_fixture():
+    
+    lrc_model = joblib.load(f"{constants.MODELS_DIR}/logistic_model.pkl")
+    rfc_model = joblib.load(f"{constants.MODELS_DIR}/rfc_model.pkl")
+    yield lrc_model, rfc_model
 
 
 def test_read_data(df_fixture):
@@ -51,16 +64,35 @@ def test_perform_feature_engineering(df_fixture):
         pytest.fail(f"Function raised an exception: {err}")
 
 
-def test_train_models():
+def test_train_models(data_fixture):
     """test train_models function"""
-    assert f"{constants.MODELS_DIR}/logistic_model.pkl"
+    x_train, x_test, y_train, y_test, x_data = data_fixture
+    try:    
+        train_models(x_train, x_test, y_train, y_test, x_data)
+    except Exception as err:
+        pytest.fail(f"Function raised an exception: {err}")
 
 
-def test_feature_importance_plot():
+def test_feature_importance_plot(data_fixture, models_fixture):
     """test feature_importance_plot function"""
-    assert f"{constants.RESULTS_DIR}/feature_importance.png"
+    _, _, _, _, x_data = data_fixture
+    _, rfc_model = models_fixture
+    try:
+        feature_importance_plot(rfc_model, x_data)
+    except Exception as err:
+        pytest.fail(f"Function raised an exception: {err}")
 
 
-def test_classification_report_image():
+def test_classification_report_image(data_fixture, models_fixture):
     """test classification_report_image function"""
-    assert f"{constants.RESULTS_DIR}/logistic_regression_report.png"
+    x_train, x_test, y_train, y_test, _ = data_fixture
+    lrc_model, rfc_model = models_fixture
+    y_train_preds_rf = rfc_model.predict(x_train)
+    y_test_preds_rf = rfc_model.predict(x_test)
+
+    y_train_preds_lr = lrc_model.predict(x_train)
+    y_test_preds_lr = lrc_model.predict(x_test)
+    try:
+        classification_report_image(y_train, y_test, y_train_preds_lr, y_train_preds_rf, y_test_preds_lr, y_test_preds_rf)
+    except Exception as err:
+        pytest.fail(f"Function raised an exception: {err}")
